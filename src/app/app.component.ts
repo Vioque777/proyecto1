772a9a40eb608ee14299daacdb70c6116b454a42
David <david_777@hotmail.com>
2022-02-11 10:43:00 +0100

    commit inicial

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'proyectoA';
+  title = 'Contador';
+  num: number = 0;
+
 }
