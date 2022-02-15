7b64baf3cffcb0226b4fa7c440e5bd05de9aef25
David <david_777@hotmail.com>
2022-02-15 10:35:18 +0100

    Final tema10

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,16 +5,35 @@ import { AppComponent } from './app.component';
 import { PrincipalComponent } from './principal/principal.component';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
+import { HijoprComponent } from './hijopr/hijopr.component';
+import { FormsModule } from '@angular/forms';
+import { CSeccion2Component } from './c-seccion2/c-seccion2.component';
+import { CSeccion3Component } from './c-seccion3/c-seccion3.component';
 
+import { RouterModule, Routes } from '@angular/router';
+
+const routes: Routes = [
+  {path:'',component:CSeccion2Component},
+  {path:'seccion2',component:CSeccion2Component},
+  {path:'seccion3',component:CSeccion3Component},
+  {path:'seccion2/:nombre',component:CSeccion2Component},
+  {path:'seccion3/:ciudad/:poblacion',component:CSeccion3Component},
+  {path:'**',component: CSeccion2Component},
+]
 @NgModule({
   declarations: [
     AppComponent,
     PrincipalComponent,
     HeaderComponent,
-    FooterComponent
+    FooterComponent,
+    HijoprComponent,
+    CSeccion2Component,
+    CSeccion3Component
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [PrincipalComponent]
